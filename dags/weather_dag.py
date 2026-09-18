b1ea4c8159afbb5184c5eeb24c0547cf02e54a8b
Caio Negrão <73142478+CaioNeg@users.noreply.github.com>
2026-09-18 00:33:20 +0000

    config init.py

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,44 +1,55 @@
+import sys
 import os
-import logging
+sys.path.insert(0, '/opt/airflow')
+
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+from pathlib import Path
+
+sys.path.insert(0, '/opt/airflow/src')
+
+from src.pipeline_weather.extract_data import extract_weather_data  
+from src.pipeline_weather.transform_data import data_transformations
+from src.pipeline_weather.load_data import load_weather_data
 from dotenv import load_dotenv
 
-# Carrega o .env automaticamente da raiz do projeto
-load_dotenv()
-
+env_path = Path(__file__).resolve().parent.parent / 'config' / '.env'
+load_dotenv(env_path)
 
 API_KEY = os.getenv('API_KEY')
-print(f"--- TESTE: Minha API KEY é: {API_KEY} ---")
-
-# Só DEPOIS de carregar o ambiente, importamos os módulos
-from pipeline_weather.extract_data import extract_weather_data
-from pipeline_weather.transform_data import data_transformations
-from pipeline_weather.load_data import load_weather_data
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={API_KEY}'
-table_name = 'sp_weather'
 
-def pipeline():
-    try:
-        logging.info("ETAPA 1: Extração de dados")
-        data = extract_weather_data(url)
-
-        logging.info("ETAPA 2: Transformação de dados")
-        df = data_transformations(data)
-
-        logging.info("ETAPA 3: Carregamento de dados")
-        load_weather_data(table_name, df)
-
-        print("\n" + "="*60)
-        print("Pipeline concluído com sucesso!")
-        print("="*60)
-
-    except Exception as e:
-        logging.error(f"Erro na execução do pipeline: {e}")
-        import traceback
-        traceback.print_exc()
-
-if __name__ == "__main__":
-    pipeline()
+@dag(
+    dag_id='weather_pipeline',
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'retries': 2,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='Pipeline ETL - CLima SP',
+    schedule='0 */1 * * * ',
+    start_date=datetime(2026, 2, 7),
+    catchup=False,
+    tags=['weather', 'etl', 'se inscreve no canal!']
+)
+def weather_pipeline():
     
+    @task
+    def extract():
+        extract_weather_data(url)
+        
+    @task
+    def transform():
+        df = data_transformations()
+        df.to_parquet('/opt/airflow/data/temp_data.parquet', index=False)
+        
+    @task 
+    def load():
+        import pandas as pd
+        df = pd.read_parquet('/opt/airflow/data/temp_data.parquet')
+        load_weather_data('sp_weather', df)
+        
+    extract() >> transform() >> load()
+
+weather_pipeline()
